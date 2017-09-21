d66a78e1416bc7dae4103f8d1e587fb14b46b027
NTDdoubled2 <32153586+NTDdoubled2@users.noreply.github.com>
2017-09-21 18:19:12 +0700

    Add files via upload

---
+++ gui.js
@@ -0,0 +1,74 @@
+﻿// Các xử lý kịch bản cho layout.html
+
+var def="---Từ khóa---";
+var emp="";
+
+function setTextKeyword(fn)
+{
+	// Hiện thị dòng chữ từ khóa trong ô tìm kiếm
+	fn.txtKeyword.value=def;
+	
+}
+	// click vào tìm kiếm mất def
+function setKeyword(fn, isClick)
+{
+	var value=fn.txtKeyword.value;
+	if (isClick) 
+	{
+		// Nhấn chuột vào
+		if(value.trim()==def)
+		{
+			fn.txtKeyword.value=emp;
+		}
+	}else
+	{
+		//thoát chuột ra
+		if(value.trim()==emp)
+		{
+			fn.txtKeyword.value=def;
+		}
+	}
+}
+
+function checkValidKeyword(fn)
+{
+	var keyword=fn.txtKeyword.value;
+	
+	keyword= keyword.trim()
+	if((keyword==def)||(keyword==emp))
+	{
+		var message = "Nhập vào từ kháo cần tìm kiếm.";
+		window.alert(message);
+		//sau khi hiện message. con trỏ sẽ tự di chuyển đến ô tìm kiếm
+		fn.txtKeyword.focus();
+		fn.txtKeyword.select();
+		fn.txtKeyword.value=emp;
+		
+	}	
+		
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
